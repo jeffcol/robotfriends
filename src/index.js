95b92507347f7cae4a2a8ebd3dc7820a21d939c8
Jefferson Villarreal <villa8ks@gmail.com>
2020-06-15 17:14:02 -0500

    cards

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "tachyons";
-import Card from "./Card";
+import CardList from "./CardList";
 import * as serviceWorker from "./serviceWorker";
 import { robots } from "./robots.js";
 
@@ -12,16 +12,10 @@ import { robots } from "./robots.js";
 ReactDOM.render(
   <React.StrictMode>
     <Fragment>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      {/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-      <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
-      <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
-      <Card id={robots[6].id} name={robots[6].name} email={robots[6].email} />
-      <Card id={robots[7].id} name={robots[7].name} email={robots[7].email} />
-      <Card id={robots[8].id} name={robots[8].name} email={robots[8].email} />
-      <Card id={robots[9].id} name={robots[9].name} email={robots[9].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} /> */}
+      <CardList robots = {robots}/>
     </Fragment>
   </React.StrictMode>,
   document.getElementById("root")
